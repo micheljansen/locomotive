da7768191c74e65becc245e6865aba0d7b38e797
Michel Jansen <michel.jansen@nedap.com>
2008-08-04 10:42:52 +0200

    begintoestand

--- vendor/plugins/helloworld/lib/locomotive/plugins/helloworld.rb
+++ vendor/plugins/helloworld/lib/locomotive/plugins/helloworld.rb
@@ -1,16 +1,14 @@
 # Helloworld
 module Locomotive::Plugins
-  module HelloWorld
-    
-    include Locomotive::Plugin
-    
-    short_name "HelloWorld"
-    #long_name "Simple HelloWorld plugin"
-    #description "Who could ever do without a HelloWorld plugin."
-    
-    #menu_path "Stuff" "HelloWorld"
-    
-    
 
-  end
+  class HelloWorld
+    include Locomotive::Plugin
+  
+    short_name "hello_world"
+    long_name "Simple HelloWorld plugin"
+    description "Who could ever do without a HelloWorld plugin."
+  
+    #menu_path "Stuff" "HelloWorld"
+  end    
+
 end  
