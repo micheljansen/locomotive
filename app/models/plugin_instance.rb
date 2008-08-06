4d805848386713a9be4e1fc34c6a90237de559d5
Michel Jansen <mail@micheljansen.org>
2008-08-06 10:01:09 +0200

    annotated models

--- app/models/plugin_instance.rb
+++ app/models/plugin_instance.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 20080805132828
+#
+# Table name: plugin_instances
+#
+#  id          :integer         not null, primary key
+#  plugin_type :string(255)     
+#  version     :integer         
+#  name        :string(255)     
+#  created_at  :datetime        
+#  updated_at  :datetime        
+#
+
 class PluginInstance < ActiveRecord::Base
   has_many :plugin_properties
   
--- app/models/plugin_property.rb
+++ app/models/plugin_property.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 20080805132828
+#
+# Table name: plugin_properties
+#
+#  id                 :integer         not null, primary key
+#  key                :string(255)     
+#  value              :string(255)     
+#  created_at         :datetime        
+#  updated_at         :datetime        
+#  plugin_instance_id :integer         
+#
+
 class PluginProperty < ActiveRecord::Base
   belongs_to :plugin_instance
 end
