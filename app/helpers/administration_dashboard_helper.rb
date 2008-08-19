3ba4059579172a01398e94c776b5456ba9aa3343
Michel Jansen <mail@micheljansen.org>
2008-08-19 13:58:58 +0200

    revamped dashboard to have more intelligent text

--- app/helpers/administration_dashboard_helper.rb
+++ app/helpers/administration_dashboard_helper.rb
@@ -1,2 +1,10 @@
 module AdministrationDashboardHelper
+  
+  # returns a humanized count string for an ActiveRecord model object
+  # like "2 beers", "1 user" or "no life"
+  def count_objects(type)
+    counter = (type.count == 0 ? "no" : type.count.to_s)
+    return (counter + " " + type.inflected)
+  end
+  
 end
