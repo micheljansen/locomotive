f370066bf9ea4e19cea58f62de1c429dc658f6ed
Michel Jansen <mail@micheljansen.org>
2008-08-21 15:43:25 +0200

    created basic roled definitions

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080821125223) do
+ActiveRecord::Schema.define(:version => 20080821125506) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
@@ -61,10 +61,7 @@ ActiveRecord::Schema.define(:version => 20080821125223) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "service_id"
   end
-
-  add_index "releases", ["service_id"], :name => "index_releases_on_service_id"
 
   create_table "roles", :force => true do |t|
     t.string   "name"
