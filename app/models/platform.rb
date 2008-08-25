93fa77829c7301f1543b343a15ae34c8bf603117
Michel Jansen <mail@micheljansen.org>
2008-08-25 15:58:41 +0200

    platform model now knows how to find other servers

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -13,4 +13,10 @@ class Platform < ActiveRecord::Base
   has_many :service_instances
   has_many :platform_memberships, :dependent => :destroy
   has_many :servers, :through => :platform_memberships
+  
+  # a list of servers not part of this platform
+  def other_servers
+    # TODO: handle this via SQL instead of substracting sets in Ruby
+    Server.find(:all)-servers
+  end
 end
