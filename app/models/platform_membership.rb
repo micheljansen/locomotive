6a40125ea7fbb3ba6d48e0bc7cd0157cafdf2fb3
Michel Jansen <mail@micheljansen.org>
2008-08-26 14:23:40 +0200

    added platform_membership validation code

--- app/models/platform_membership.rb
+++ app/models/platform_membership.rb
@@ -13,4 +13,19 @@
 class PlatformMembership < ActiveRecord::Base
   belongs_to :server
   belongs_to :platform
+  
+  validates_presence_of :server_id
+  validates_presence_of :platform_id
+  
+  validates_associated :server, :on => :save
+  validates_associated :platform, :on => :save
+  
+  # workaround rails bug #5369
+  # causing validates_associated not to fail on nonexisting id's
+  validates_each :server, :platform do |record, attr, value|
+       record.errors.add attr, 'is invalid' if value.nil?
+  end
+  
+  validates_uniqueness_of :server_id, :scope => :platform_id, 
+                          :message => "already belongs to this platform"
 end
