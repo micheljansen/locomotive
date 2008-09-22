a76fc2562495a5515768f39250ffe70b98490329
Michel Jansen <mail@micheljansen.org>
2008-09-22 11:50:53 +0200

    plugin loading mechanism now actually loads the plugins

--- lib/locomotive/plugins.rb
+++ lib/locomotive/plugins.rb
@@ -1,6 +1,8 @@
 module Locomotive
   module Plugins
     
+    DEFAULT_PATH = File.join(RAILS_ROOT, 'vendor','plugins','locomotive')
+    
     class << self
       attr_accessor :discovered
     end
@@ -16,14 +18,14 @@ module Locomotive
             
       installed_plugins.each do |p|
         puts "\t#{p}"
-        
+        require File.join(DEFAULT_PATH, p, 'lib', 'locomotive', 'plugins', p)
       end
     end
     
     # Generates a list of installed plugins from the directory structure
     # in vendor/plugins/locomotive.
     def self.installed_plugins
-      base_path = File.join(RAILS_ROOT, 'vendor','plugins','locomotive')
+      base_path = Locomotive::Plugins::DEFAULT_PATH
       plugin_paths = Dir[File.join(base_path, '*')].select {|d| File.directory?(d)}
       plugin_names = plugin_paths.map {|d| File.basename(d)}
     end
