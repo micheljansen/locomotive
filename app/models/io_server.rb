d77bf4cdc9c5009396e4d3b6273a45281d265622
Michel Jansen <mail@micheljansen.org>
2008-12-09 11:58:22 +0100

    overrided self.deploy in io_server servicetype with empty skeleton implementation

--- app/models/io_server.rb
+++ app/models/io_server.rb
@@ -4,4 +4,10 @@ class IOServer < ServiceType
   depends_on "appserver"
   
 
+  def self.deploy(service_instance)
+    puts "iO Deploy called for 
+            release #{service_instance.release.name} 
+            for client #{service_instance.client.name}"
+  end
+
 end
