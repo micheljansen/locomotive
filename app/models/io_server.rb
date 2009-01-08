55d67e40be2677b35e551947c4f5827d6f0a0cdc
Michel Jansen <mail@micheljansen.org>
2009-01-08 16:01:23 +0100

    removed URL

--- app/models/io_server.rb
+++ app/models/io_server.rb
@@ -5,7 +5,6 @@ class IOServer < ServiceType
   
 
   def self.deploy(service_instance)
-    #http://10.17.16.35/iomonitor/remotecontrollers/update_controller.asp?pass=test&action=update&customer_code=AA0001&server_name=NVC0945&servicestart=&activation=Caress,ImportWatchAndGo,Planning&port=9000&rmi=123&jmxrmi=123&jndi=123&memory=0
     puts "iO Deploy called for 
             release #{service_instance.release.name} 
             for client #{service_instance.client.name}"
