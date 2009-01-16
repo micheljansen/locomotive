9af822015ad5a0c96de9203ee50b21256e2cfcca
Alex Coles <alex@alexcolesportfolio.com>
2009-01-16 14:22:12 +0100

    Begin working on the switch from AR to DataMapper
    * Make required changes to config/environment.rb.
    * Add rails_datamapper plugin to vendor/plugins, commenting out
      one line to avoid gem activation errors (because the plugin is
      somewhat out-of-date).
    * Remove annotate_models plugin, as it requires AR.
    
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- config/environment.rb
+++ config/environment.rb
@@ -21,17 +21,23 @@ Rails::Initializer.run do |config|
   # Skip frameworks you're not going to use. To use Rails without a database
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  config.frameworks -= [ :active_record ]
 
-  # Specify gems that this application depends on. 
+  # Specify gems that this application depends on.
   # They can then be installed with "rake gems:install" on new installations.
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "addressable", :lib => "addressable/uri"
+  config.gem "data_objects"
+  config.gem "do_sqlite3"
+  config.gem "dm-core"
 
-  # Only load the plugins named here, in the order given. By default, all plugins 
+  # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  config.plugins = [ :rails_datamapper, :all ]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
@@ -47,7 +53,7 @@ Rails::Initializer.run do |config|
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
+  # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => '_panel_session',
---
+++ vendor/plugins/rails_datamapper/init.rb
@@ -0,0 +1,4 @@
+#gem 'dm-core', '~>0.9.10'
+require 'dm-core'
+require File.expand_path(File.dirname(__FILE__) + '/lib/rails_datamapper')
+create_connection()
