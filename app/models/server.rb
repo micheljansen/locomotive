f4e23a697c99f777dcd39851888e402ccd6664c3
Alex Coles <alex@alexcolesportfolio.com>
2009-01-27 15:33:10 +0100

    Add validation to Server properties, create specs
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- app/models/server.rb
+++ app/models/server.rb
@@ -3,9 +3,10 @@ require 'ping'
 class Server
   include DataMapper::Resource
 
-  property :id, Integer, :serial => true
-  property :name, String
-  property :hostname, String
+  property :id,         Integer, :serial => true
+  property :name,       String,  :nullable => false
+  property :hostname,   String,  :nullable => false, :length => 2..255,
+                                 :format => Proc.new { |code| code =~ /(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\\-]*[A-Za-z0-9])$/ }
   property :created_at, DateTime
   property :updated_at, DateTime
 
---
+++ spec/models/server_spec.rb
@@ -0,0 +1,53 @@
+require File.join( File.dirname(__FILE__), '..', "spec_helper" )
+
+describe Server do
+
+  it "should not be valid without a name" do
+    server = Server.new
+    server.should_not be_valid
+  end
+  
+  it "should be valid with a name and hostname" do
+    server = Server.new
+    server.name = 'Windows 2000'
+    server.hostname = 'millenium-windoze'
+    server.should be_valid
+  end
+
+  describe 'hostname' do
+
+    before(:each) do
+      @server = Server.new
+      @server.name = 'Ubuntu 8.10'
+    end
+
+    it 'should be greater than 2' do
+      @server.hostname = 'u'
+      @server.should_not be_valid
+    end
+
+    it 'should be less than 255' do
+      @server.hostname = 'averylonghostnameforaveryexpensivebladeserverexceptthatithasahostnamethatdoesnotmeetthecriterialaiddownbyanrfc.whatreallyneedstohappenisthatitgetssentbacktothevendor.letsletouritmanagerknowaboutit-becauseitmanagersarereallygoodatbeingintheknow.ofcourseofcourseofcourseofcourse'
+      @server.should_not be_valid
+    end
+
+    it 'should only contain ASCII characters A-Z, digits, or hyphens' do
+      @server.hostname = '@!:<>?!"'
+      @server.should_not be_valid
+      @server.hostname = 'asa'
+      @server.should be_valid
+    end
+
+    it 'should not begin or end with a hyphen' do
+      pending "regex not yet picking up on this"
+      @server.hostname = 'windows-box'
+      @server.should be_valid
+      @server.hostname = '-windows-box'
+      @server.should be_valid
+      @server.hostname = 'windows-box-'
+      @server.should be_valid
+    end
+
+  end
+
+end
