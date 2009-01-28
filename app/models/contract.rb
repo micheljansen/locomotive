4250f51fb6e05a84b52bfdeb5c148687e0cf81b5
Alex Coles <alex@alexcolesportfolio.com>
2009-01-28 10:19:42 +0100

    Re-add indexes to Contract properties
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,14 +1,11 @@
 class Contract
   include DataMapper::Resource
 
-  property :id, Integer, :serial => true
-  property :client_id, Integer
-  property :service_id, Integer
+  property :id,         Integer, :serial => true
+  property :client_id,  Integer, :index => true
+  property :service_id, Integer, :index => true
   property :created_at, DateTime
   property :updated_at, DateTime
-
-  # add_index "contracts", ["client_id"], :name => "index_contracts_on_client_id"
-  # add_index "contracts", ["service_id"], :name => "index_contracts_on_service_id"
 
   belongs_to :client
   belongs_to :service
