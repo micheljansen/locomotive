3251c9059e85e3da320908fc2038167f291832d3
Alex Coles <alex@alexcolesportfolio.com>
2009-02-04 18:22:41 +0100

    Fix inflection of model names on administration dashboard page
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- app/helpers/administration_dashboard_helper.rb
+++ app/helpers/administration_dashboard_helper.rb
@@ -2,11 +2,13 @@ module Merb
   module Locomotive
     module AdministrationDashboardHelper
 
-      # returns a humanized count string for an ActiveRecord model object
+      # returns a humanized count string for a DataObjects model object
       # like "2 beers", "1 user" or "no life"
       def count_objects(type)
-        counter = (type.count == 0 ? "no" : type.count.to_s)
-        return (counter + " " + type.name) # FIXME: reinstate inflections .inflected
+        count = type.count
+        counter = (count == 0 ? "no" : count.to_s)
+        type_name = Extlib::Inflection.demodulize(type.name)
+        return (counter + " " + (count < 1 || count > 1 ? type_name.pluralize : type_name))
       end
 
     end
