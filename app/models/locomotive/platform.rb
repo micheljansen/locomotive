micheljansen/locomotive

commit 2eaa617b41b990eb37c7a5abc948bfe67ac9b2f3

Rename Server to System, update associated models/controllers
* System is a more generic term -- Locomotive is primarily for managing
  what are traditionally known as servers (web servers, database servers)
  and deploying web applications, but should be capable of connecting to/
  managing any system that is part of a deployment platform.

Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

app/models/locomotive/platform.rb
@@ -9,7 +9,7 @@ module Locomotive
 
     has n, :deployments
     has n, :platform_memberships # :dependent => :destroy
-    has n, :servers, :through => :platform_memberships
+    has n, :systems, :through => :platform_memberships
 
     validates_present :name
 
@@ -28,8 +28,8 @@ module Locomotive
 
     # returns the membership for a given server
     # or nil if not found
-    def membership_for_server_id(server_id)
-      platform_memberships.all(:server_id => server_id)
+    def membership_for_system_id(system_id)
+      platform_memberships.all(:system_id => system_id)
     end
   end
 end

app/views/locomotive/servers/show.html.haml
@@ -1,42 +1,0 @@
-%p
-  %b
-    Name:
-  = h @server.name
-%p
-  %b
-    Hostname:
-  = h @server.hostname
-%p
-  %strong
-    Operating System:
-  = h @server.operating_system.name
-%p
-  %b
-    Roles:
-  %ul
-    - @server.roles.each do |role|
-      %li
-        = h role.name
-
-= link_to 'Back to server list', resource(:servers)
-
-- throw_content(:sidebar) do
-  %h2
-    Actions
-  %ul#specific
-    %li
-      = link_to 'Edit this server', resource(@server, :edit)
-    %li
-      = link_to 'Assign a role', resource(@server, :purposes, :new)
-  %ul#general
-    %li
-      = link_to 'New server', resource(:servers, :new)
-  - if @server.others.size > 0
-    %h2
-      More servers
-    %ul
-      - @server.others.each do |server|
-        %li
-          = link_to server.name, server
-      %li
-        = link_to "See all...", resource(:servers)

app/views/locomotive/systems/show.html.haml
@@ -1,0 +1,42 @@
+%p
+  %b
+    Name:
+  = h @system.name
+%p
+  %b
+    Hostname:
+  = h @system.hostname
+%p
+  %strong
+    Operating System:
+  = h @system.operating_system.name
+%p
+  %b
+    Roles:
+  %ul
+    - @system.roles.each do |role|
+      %li
+        = h role.name
+
+= link_to 'Back to system list', resource(:systems)
+
+- throw_content(:sidebar) do
+  %h2
+    Actions
+  %ul#specific
+    %li
+      = link_to 'Edit this system', resource(@system, :edit)
+    %li
+      = link_to 'Assign a role', resource(@system, :purposes, :new)
+  %ul#general
+    %li
+      = link_to 'New system', resource(:systems, :new)
+  - if @system.others.size > 0
+    %h2
+      More systems
+    %ul
+      - @system.others.each do |system|
+        %li
+          = link_to system.name, system
+      %li
+        = link_to "See all...", resource(:systems)

app/controllers/deployments.rb
@@ -108,7 +108,7 @@ module Locomotive
     end
 
     def delete(id)
-      @deployment = Locomotive::ServerInstance.get(id)
+      @deployment = Locomotive::Deployment.get(id)
       raise NotFound unless @deployment
       display @deployment
     end

app/controllers/platform_memberships.rb
@@ -91,17 +91,17 @@ module Locomotive
     end
 
     def find_server
-      @server ||= Locomotive::Server.get(params[:server_id]) if params[:server_id]
+      @system ||= Locomotive::System.get(params[:system_id]) if params[:system_id]
     end
 
     def initialize_params
       puts "initializing params"
       # y params
       #     p @platform
-      #     p @server
+      #     p @system
       params[:platform_membership] ||= {}
       params[:platform_membership][:platform_id] ||= @platform.id if @platform
-      params[:platform_membership][:server_id] ||= @server.id if @server
+      params[:platform_membership][:system_id] ||= @system.id if @system
       #y params
     end
   end

app/controllers/purposes.rb
@@ -7,7 +7,7 @@ module Locomotive
   #  # GET /purposes
   #  # GET /purposes.xml
     def index
-      @purposes = @server.purposes
+      @purposes = @system.purposes
       display @purposes
     end
 
@@ -24,7 +24,7 @@ module Locomotive
     def new
       only_provides :html
       @purpose = Locomotive::Purpose.new
-      @purpose.server_id = @server.id
+      @purpose.system_id = @system.id
       display @purpose
     end
 
@@ -41,7 +41,7 @@ module Locomotive
       @purpose = Locomotive::Purpose.new(purpose)
 
       if @purpose.save
-        redirect resource(@server), :message => {:notice => "Role was successfully created"}
+        redirect resource(@system), :message => {:notice => "Role was successfully created"}
       else
         render :new
       end
@@ -53,7 +53,7 @@ module Locomotive
       @purpose = Locomotive::Purpose.get(id)
       raise NotFound unless @purpose
       if @purpose.update_attributes(purpose)
-        redirect resource(@server), :message => {:notice => 'Role was successfully updated'}
+        redirect resource(@system), :message => {:notice => 'Role was successfully updated'}
       else
         render :edit
       end
@@ -71,7 +71,7 @@ module Locomotive
       @purpose = Locomotive::Purpose.get(id)
       raise NotFound unless @purpose
       if @purpose.destroy
-        redirect resource(@server)
+        redirect resource(@system)
       else
         raise InternalServerError
       end
@@ -81,11 +81,11 @@ module Locomotive
     # through a route containing a server, like
     # /servers/1/purposes/x
     def find_associated_server
-      @server = nil
-      server_id = params[:server_id]
+      @system = nil
+      system_id = params[:system_id]
 
-      unless server_id.nil?
-        @server = Locomotive::Server.get(server_id)
+      unless system_id.nil?
+        @system = Locomotive::System.get(system_id)
       end
     end
 
@@ -93,11 +93,11 @@ module Locomotive
     # through a route containing a server, like
     # /servers/1/roles
     def find_associated_server
-      @server = nil
-      server_id = params[:server_id]
+      @system = nil
+      system_id = params[:system_id]
 
-      unless server_id.nil?
-        @server = Locomotive::Server.get(server_id)
+      unless system_id.nil?
+        @system = Locomotive::System.get(system_id)
       end
     end
 

app/controllers/servers.rb
@@ -1,82 +1,0 @@
-module Locomotive
-  class Servers < Locomotive::Application
-
-    before :init_menu
-
-    # GET /servers
-    # GET /servers.xml
-    def index
-      @page_count, @servers = Locomotive::Server.paginated(:page => (params[:page] || 1).to_i)
-      display @servers
-    end
-
-    # GET /servers/1
-    # GET /servers/1.xml
-    def show(id)
-      @server = Locomotive::Server.get(id)
-      raise NotFound unless @server
-      display @server
-    end
-
-    # GET /servers/new
-    def new
-      only_provides :html
-      @server = Locomotive::Server.new
-      display @server
-    end
-
-    # GET /servers/1/edit
-    def edit(id)
-      only_provides :html
-      @server = Locomotive::Server.get(id)
-      raise NotFound unless @server
-      display @server
-    end
-
-    # POST /servers
-    # POST /servers.xml
-    def create(server)
-      @server = Locomotive::Server.new(server)
-
-      if @server.save
-        redirect resource(@server), :message => {:notice => "Server was successfully created"}
-      else
-        render :new
-      end
-    end
-
-    # PUT /servers/1
-    # PUT /servers/1.xml
-    def update(id, server)
-      @server = Locomotive::Server.get(id)
-      raise NotFound unless @server
-      if @server.update_attributes(server)
-        redirect resource(@server), :message => {:notice => "Server was successfully updated"}
-      else
-        render :edit
-      end
-    end
-
-    def delete(id)
-      @server = Locomotive::Server.get(id)
-      raise NotFound unless @server
-      display @server
-    end
-
-    # DELETE /servers/1
-    # DELETE /servers/1.xml
-    def destroy(id)
-      @server = Locomotive::Server.get(id)
-      raise NotFound unless @server
-      if @server.destroy
-        redirect resource(:servers)
-      else
-        raise InternalServerError
-      end
-    end
-
-    def init_menu
-      @menu = ["Administration","Locomotive::Servers"]
-    end
-  end
-end

app/controllers/systems.rb
@@ -1,0 +1,82 @@
+module Locomotive
+  class Systems < Locomotive::Application
+
+    before :init_menu
+
+    # GET /systems
+    # GET /systems.xml
+    def index
+      @page_count, @systems = Locomotive::System.paginated(:page => (params[:page] || 1).to_i)
+      display @systems
+    end
+
+    # GET /systems/1
+    # GET /systems/1.xml
+    def show(id)
+      @system = Locomotive::System.get(id)
+      raise NotFound unless @system
+      display @system
+    end
+
+    # GET /systems/new
+    def new
+      only_provides :html
+      @system = Locomotive::System.new
+      display @system
+    end
+
+    # GET /systems/1/edit
+    def edit(id)
+      only_provides :html
+      @system = Locomotive::System.get(id)
+      raise NotFound unless @system
+      display @system
+    end
+
+    # POST /systems
+    # POST /systems.xml
+    def create(system)
+      @system = Locomotive::System.new(system)
+
+      if @system.save
+        redirect resource(@system), :message => {:notice => "System was successfully created"}
+      else
+        render :new
+      end
+    end
+
+    # PUT /systems/1
+    # PUT /systems/1.xml
+    def update(id, system)
+      @system = Locomotive::System.get(id)
+      raise NotFound unless @system
+      if @system.update_attributes(system)
+        redirect resource(@system), :message => {:notice => "System was successfully updated"}
+      else
+        render :edit
+      end
+    end
+
+    def delete(id)
+      @system = Locomotive::System.get(id)
+      raise NotFound unless @system
+      display @system
+    end
+
+    # DELETE /systems/1
+    # DELETE /systems/1.xml
+    def destroy(id)
+      @system = Locomotive::System.get(id)
+      raise NotFound unless @system
+      if @system.destroy
+        redirect resource(:systems)
+      else
+        raise InternalServerError
+      end
+    end
+
+    def init_menu
+      @menu = ["Administration","Locomotive::Systems"]
+    end
+  end
+end

commit ae98feefa02350a2846a106165f01606c8bb8790

Fix Platform management UI broken in commit 2eaa617
Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

app/models/locomotive/platform.rb
@@ -15,10 +15,10 @@ module Locomotive
 
     is_paginated
 
-    # a list of servers not part of this platform
-    def other_servers
+    # a list of systems not part of this platform
+    def other_systems
       # TODO: handle this via SQL instead of substracting sets in Ruby
-      Server.all - servers
+      System.all - systems
     end
 
     # a list of all platforms except this one
@@ -26,7 +26,7 @@ module Locomotive
       Platform.all - [self]
     end
 
-    # returns the membership for a given server
+    # returns the membership for a given system
     # or nil if not found
     def membership_for_system_id(system_id)
       platform_memberships.all(:system_id => system_id)

app/views/locomotive/platforms/_membership.html.haml
@@ -1,6 +1,6 @@
 %li.membership{ :id => "membership_#{h membership.id}" }
   %strong
-    = h membership.server.name
+    = h membership.system.name
   \-
   %em
-    = h membership.server.hostname
+    = h membership.system.hostname

app/views/locomotive/platforms/_memberships.html.haml
@@ -1,6 +1,6 @@
 %h2
   = h platform.name
-#servers_options
+#systems_options
   = link_to "Edit...", resource(platform, :edit)
 %ul.connected-sortable
 - unless memberships.nil?

app/views/locomotive/platforms/_other_systems.html.haml
@@ -1,10 +1,10 @@
 %h2
-  Other servers
-#other_servers_options
+  Other systems
+#other_systems_options
   = link_to "Add more...", resource(:systems, :new)
   %a#filter{ :href => "#" }
     Filter
-  #server_filter_box{ :style => "display: none" }
+  #system_filter_box{ :style => "display: none" }
     %span
       Show only
     %span{ :style => "float: right" }
@@ -14,10 +14,10 @@
 %script{ :type => "text/javascript" }
   $(function() {
   $("#filter").click(function() {
-  $("#server_filter_box").toggle("blind");
+  $("#system_filter_box").toggle("blind");
   });
   });
 
 %ul.connected-sortable
-  - unless other_servers.nil?
-    = partial :server, :with => other_servers, :as => :server
+  - unless other_systems.nil?
+    = partial :system, :with => other_systems, :as => :system

app/views/locomotive/platforms/_server.html.haml
@@ -1,6 +1,0 @@
-%li.server{ :id => "server_#{h server.id}" }
-  %strong
-    = h server.name
-  \-
-  %em
-    = h server.hostname

app/views/locomotive/platforms/_system.html.haml
@@ -1,0 +1,6 @@
+%li.system{ :id => "system_#{h system.id}" }
+  %strong
+    = h system.name
+  \-
+  %em
+    = h system.hostname

app/views/locomotive/platforms/show.html.haml
@@ -2,13 +2,13 @@
   Editing platform
   = h @platform.name
 %p
-  Drag servers between your into this platform to add them
+  Drag systems between your into this platform to add them
   or drag them away to remove.
 #platform_manager
-  #servers
+  #systems
     = partial :memberships, :platform_memberships => @platform.platform_memberships, :platform => @platform
-  #other_servers
-    = partial :other_servers, :other_servers => @platform.other_servers, :platform => @platform
+  #other_systems
+    = partial :other_systems, :other_systems => @platform.other_systems, :platform => @platform
   %div{ :style => "clear: both" }
   %style{ :type => "text/css" }
     \/* TODO: move to an actual stylesheet */
@@ -20,7 +20,7 @@
     list-style-type: none;
     }
   %script
-    $('#servers ul, #other_servers ul').sortable({
+    $('#systems ul, #other_systems ul').sortable({
     receive: function(event, ui) {
     $(this).effect('highlight');
     \// TODO: add the ajax call
@@ -28,7 +28,7 @@
     connectWith: ['.connected-sortable']
     });
     
-    \//$('.server').draggable({
+    \//$('.system').draggable({
     \//  containment: '#platform_manager',
     \//  revert: 'invalid'
     \//});
@@ -37,15 +37,15 @@
     \//  revert: 'invalid'
     \//});
     
-    \//$('#servers').droppable({
-    \//  accept: '.server',
+    \//$('#systems').droppable({
+    \//  accept: '.system',
     \//  hoverClass: 'dragover',
     \//  drop: function(event, ui) {
     \//    $(this).effect('highlight');
     \//    // TODO: add the ajax call
     \//  }
     \//});
-    \//$('#other_servers').droppable({
+    \//$('#other_systems').droppable({
     \//  accept: '.membership',
     \//  hoverClass: 'dragover',
     \//  drop: function(event, ui) {

public/stylesheets/panel.css
@@ -165,7 +165,7 @@ body {
 	margin: 0;
 }
 
-#platform_manager #servers {
+#platform_manager #systems {
 	float: left;
 	display: block;
 	background: #eee;
@@ -180,7 +180,7 @@ body {
 }
 
 
-#platform_manager #other_servers {
+#platform_manager #other_systems {
 	float: right;
 	display: block;
 	background: #eee;
@@ -190,7 +190,7 @@ body {
 	min-height: 200px;
 }
 
-#platform_manager .server, #platform_manager .membership {
+#platform_manager .system, #platform_manager .membership {
 	padding: 10px;
 	background: #fee;
 	margin-top: 5px;
@@ -198,7 +198,7 @@ body {
 	cursor:move;
 }
 
-#server_filter_box {
+#system_filter_box {
 	position: relative;
 	background: #ddd;
 	border: 1px solid #ccc;
@@ -209,7 +209,7 @@ body {
 	height: 20px;
 }
 
-#server_filter_box h3 {
+#system_filter_box h3 {
 	
 }
 