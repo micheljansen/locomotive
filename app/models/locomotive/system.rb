8c88e5eaa248d0965503bdd56352ad634d601286
Alex Coles <alex@alexcolesportfolio.com>
2009-02-12 00:31:37 +0100

    Fix UI for adding/removing Roles on edit System page
    * Get checkboxes working with has n, :through =>
    * Requires some extra code in System model: roles are deleted,
      and the relationship recreated on every save.
    * Url fix on show system page.
    * Minor wording fixes on add role to server (new purpose) page.
    
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- app/models/locomotive/system.rb
+++ app/models/locomotive/system.rb
@@ -29,6 +29,10 @@ module Locomotive
 
     is_paginated
 
+    attr_accessor :role_ids
+
+    after :save, :assign_roles
+
     def others
       System.all - [self]
     end
@@ -36,6 +40,24 @@ module Locomotive
     def check
       puts "pinging #{hostname}"
       return Ping.pingecho(hostname, 10)
+    end
+
+    def delete_purposes
+      purposes.each { |p| p.destroy }
+    end
+    alias delete_roles delete_purposes
+
+    private
+
+    def assign_roles
+      unless new_record?
+        self.delete_purposes
+        self.purposes.reload
+      end
+      self.role_ids.each do |id|
+        role = Role.first_or_create(:id => id)
+        Purpose.create(:system_id => self.id, :role_id => role.id)
+      end
     end
 
   end
